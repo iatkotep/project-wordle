ad7c45d3cf36dd0e76066f069fe8598b12f326d4
David Lawson <davidlawson@Davids-MacBook-Pro-2.local>
2026-03-08 22:08:43 -0600

    chore: create Keyboard component

---
+++ src/components/Keyboard/Keyboard.jsx
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import classNames from "classnames";
+
+import { useGameContext } from "../GameContext/GameContext";
+
+const Keyboard = () => {
+  const { keyboardRows, handleKeyClick } = useGameContext();
+  return (
+    <div className="keyboard">
+      {keyboardRows.map((row, rowIdx) => (
+        <div className="keyboard-row" key={rowIdx}>
+          {row.map(({ letter, status, isActive }) => (
+            <div
+              className={classNames("keyboard-cell", {
+                "keyboard-cell--active": isActive,
+              })}
+              key={letter}
+              data-status={status}
+              data-isactive={isActive}
+            >
+              <button
+                className="keyboard-key"
+                onClick={handleKeyClick}
+                value={letter}
+              >
+                {letter}
+              </button>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Keyboard;
